9006354e1a2549d6d3c63b279e0304831c5010c9
jrecht <jrecht@b07142bf-ac3a-0410-9e62-2714dd94d8c8>
2009-10-01 20:38:18 +0000

    fixed issue #36: better support for parameterized builds

--- hudson-eclipse/src/dk/contix/eclipse/hudson/BuildParameter.java
+++ hudson-eclipse/src/dk/contix/eclipse/hudson/BuildParameter.java
@@ -14,12 +14,19 @@ import org.apache.commons.codec.binary.Base64;
 public class BuildParameter implements Serializable {
 	private String name;
 	private String value;
+	private String description;
 
 	public BuildParameter(String name, String value) {
 		this.name = name;
 		this.value = value;
 	}
-	
+
+	public BuildParameter(String name, String value, String description) {
+		this.name = name;
+		this.value = value;
+		this.description = description;
+	}
+
 	public BuildParameter() {
 	}
 
@@ -38,8 +45,15 @@ public class BuildParameter implements Serializable {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	
-	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public static String serialize(List<BuildParameter> params) {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		try {
